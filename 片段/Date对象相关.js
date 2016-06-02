e911c582eb318d9bb1eac34a5d38ab8bb94aad85
li9269391 <9269391@qq.com>
2016-06-02 14:32:26 +0800

    Update Date对象相关.js

--- 片段/Date对象相关.js
+++ 片段/Date对象相关.js
@@ -69,3 +69,156 @@ Date.prototype.datediff = function(strInterval, dtEnd) {
 		return dtEnd.getFullYear() - dtStart.getFullYear();
 	}
 };
+
+Date.prototype.format = function (format) {
+	var o = {
+	    "M+": this.getMonth() + 1,
+	    "d+": this.getDate(),
+	    "h+": this.getHours(),
+	    "m+": this.getMinutes(),
+	    "s+": this.getSeconds(),
+	    "q+": Math.floor((this.getMonth() + 3) / 3),
+	    "S": this.getMilliseconds()
+	}
+if (/(y+)/.test(format)) {
+    format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+}
+for (var k in o) {
+    if (new RegExp("(" + k + ")").test(format)) {
+        format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+    }
+}
+return format;
+};
+/**  
+*转换日期对象为日期字符串  
+* @param date 日期对象  
+* @param isFull 是否为完整的日期数据,  
+*               为true时, 格式如"2000-03-05 01:05:04"  
+*               为false时, 格式如 "2000-03-05"  
+* @return 符合要求的日期字符串  
+*/  
+function getSmpFormatDate(date, isFull) {
+	var pattern = "";
+	if (isFull == true || isFull == undefined) {
+	    pattern = "yyyy-MM-dd hh:mm:ss";
+	} else {
+	    pattern = "yyyy-MM-dd";
+	}
+	return getFormatDate(date, pattern);
+};
+
+/**  
+*转换当前日期对象为日期字符串  
+* @param date 日期对象  
+* @param isFull 是否为完整的日期数据,  
+*               为true时, 格式如"2000-03-05 01:05:04"  
+*               为false时, 格式如 "2000-03-05"  
+* @return 符合要求的日期字符串  
+*/  
+
+function getSmpFormatNowDate(isFull) {
+	return getSmpFormatDate(new Date(), isFull);
+};
+/**  
+*转换long值为日期字符串  
+* @param l long值  
+* @param isFull 是否为完整的日期数据,  
+*               为true时, 格式如"2000-03-05 01:05:04"  
+*               为false时, 格式如 "2000-03-05"  
+* @return 符合要求的日期字符串  
+*/  
+
+function getSmpFormatDateByLong(l, isFull) {
+	return getSmpFormatDate(new Date(l), isFull);
+};
+/**  
+*转换long值为日期字符串  
+* @param l long值  
+* @param pattern 格式字符串,例如：yyyy-MM-dd hh:mm:ss  
+* @return 符合要求的日期字符串  
+*/  
+
+function getFormatDateByLong(l, pattern) {
+	return getFormatDate(new Date(l), pattern);
+};
+/**  
+*转换日期对象为日期字符串  
+* @param l long值  
+* @param pattern 格式字符串,例如：yyyy-MM-dd hh:mm:ss  
+* @return 符合要求的日期字符串  
+*/  
+function getFormatDate(date, pattern) {
+	if (date == undefined) {
+	    date = new Date();
+	}
+	if (pattern == undefined) {
+	    pattern = "yyyy-MM-dd hh:mm:ss";
+	}
+	return date.format(pattern);
+};
+/**
+**
+*把时间字符串转成标准时间
+**/
+function str2date(s){
+s=typeof s=="number"?s+"":s;
+if (typeof(s) == "string") {
+    $.trim(s);
+    s = s.replace(/[\/\s\:]/gi, "-");
+    if (s.match(/\-/)!=null) {
+        md = s.split("-");
+        md[1] = md[1] * 1 - 1;
+        if (md.length > 3) {
+            s = new Date(md[0], md[1], md[2], md[3], md[4], md[5])
+        }
+        else {
+            s = new Date(md[0], md[1], md[2]);
+        }
+    }
+    else {
+        md = s.split("");
+        var mdy = ("" + md[0] + md[1] + md[2] + md[3]) * 1;
+        var mdm = ("" + md[4] + md[5]) * 1 - 1;
+        var mdd = ("" + md[6] + md[7]) * 1;
+        if (md.length > 10) {
+            var mdhh = ("" + md[8] + md[9]) * 1;
+            var mdmm= ("" + md[10] + md[11]) * 1;
+            var mdss = ("" + md[12] + md[13]) * 1;
+            s = new Date(mdy, mdm, mdd, mdhh, mdmm, mdss);
+        }
+        else {
+            s = new Date(mdy, mdm, mdd);
+        }
+    }
+}
+return s;
+}
+/**
+**
+*减去相应天数
+**/
+Date.prototype.descDay=function(dayNum){
+	var time=this.getTime()-dayNum*24*60*60*1000;
+	return new Date(time);
+};
+/**
+**
+*增加相应天数
+**/
+Date.prototype.addDay=function(dayNum){
+	var time=this.getTime()+dayNum*24*60*60*1000;
+	return new Date(time);
+};
+/**
+***比较小时、分钟是否到期
+**/
+Date.prototype.compareHour = function(hour,minute){
+	if (minute == undefined) {
+    minute = 0;
+}
+	var hour1 = this.getHours();
+	var minute1=this.getMinutes();
+	return (hour1*60+minute1)-(hour*60+minute);
+};
+    
