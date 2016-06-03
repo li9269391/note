936f0c153c293f14de3c66953cf11716b68b6cd9
li9269391 <9269391@qq.com>
2016-06-03 11:12:15 +0800

    Update 数字千分符（￥）.js

--- 片段/数字千分符（￥）.js
+++ 片段/数字千分符（￥）.js
@@ -2,3 +2,12 @@ Number.prototype.rmb = function(prevfix, n) {
   //	return (prevfix === false ? '' : '\uffe5') + this.toFixed(n === void 0 ? 2 : n).toString().replace(/(\d)(?=(\d{3})+($|\.))/g, '$1,');
 	return this.toFixed(n === void 0 ? 2 : n).toString().replace(/(\d)(?=(\d{3})+($|\.))/g, '$1,');
 };
+
+// 格式化千分符
+function CommaFormatted(str) {
+    return str.replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,');
+}
+jQuery('.format-txt').each(function(){
+    var txt = CommaFormatted(jQuery(this).text());
+    jQuery(this).text(txt);
+});
