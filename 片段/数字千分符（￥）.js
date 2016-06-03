ce577abaecd836a3857da1c0876dcb6544c2f963
li9269391 <9269391@qq.com>
2016-06-03 11:52:10 +0800

    Update 数字千分符（￥）.js

--- 片段/数字千分符（￥）.js
+++ 片段/数字千分符（￥）.js
@@ -1,10 +1,12 @@
-Number.prototype.rmb = function(prevfix, n) {
-  //	return (prevfix === false ? '' : '\uffe5') + this.toFixed(n === void 0 ? 2 : n).toString().replace(/(\d)(?=(\d{3})+($|\.))/g, '$1,');
-	return this.toFixed(n === void 0 ? 2 : n).toString().replace(/(\d)(?=(\d{3})+($|\.))/g, '$1,');
+Number.prototype.CommaFormatted = function(n,symbol) {
+	return (symbol || '\uffe5') + this.toFixed(n === void 0 ? 2 : n).toString().replace(/(\d)(?=(\d{3})+($|\.))/g, '$1,');
+	// return this.toFixed(n === void 0 ? 2 : n).toString().replace(/(\d)(?=(\d{3})+($|\.))/g, '$1,');
 };
+(4564546).CommaFormatted(2,"$");
 
 // 格式化千分符
-function CommaFormatted(str) {
+function CommaFormatted(str,symbol) {
+	var symbol = symbol || "￥";
     return str.replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,');
 }
 jQuery('.format-txt').each(function(){
