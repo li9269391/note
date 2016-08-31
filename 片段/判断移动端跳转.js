cd5fb68d3cfef902b1869d20604aff5b50484e96
li9269391 <9269391@qq.com>
2016-08-31 11:51:05 +0800

    Update 判断移动端跳转.js

--- 片段/判断移动端跳转.js
+++ 片段/判断移动端跳转.js
@@ -9,7 +9,21 @@ String.prototype.getParam = function(n){
         return typeof(m[0].split(r1)[1])=='undefined'?'':decodeURIComponent(m[0].split(r1)[1]);
     }
 };
-if(location.search.getParam("from")!="wap"){
-    function mobile_device_detect(url){var thisOS=navigator.platform;var os=new Array("iPhone","iPod","android","Nokia","SymbianOS","Symbian","Windows Phone","Phone","Linux armv71","MAUI","UNTRUSTED/1.0","Windows CE","BlackBerry","IEMobile");for(var i=0;i<os.length;i++){if(thisOS.match(os[i])){window.location=url;}}var check=navigator.appVersion;if(check.match(/linux/i)){if(check.match(/mobile/i)||check.match(/X11/i)){window.location=url;}}}
-    mobile_device_detect("/m/index.html");    // 跳转链接
+function mobile_device_detect(url){
+        var thisOS=navigator.platform;
+        var os=new Array("iPhone","iPod","android","Nokia","SymbianOS","Symbian","Windows Phone","Phone","Linux armv71","MAUI","UNTRUSTED/1.0","Windows CE","BlackBerry","IEMobile");
+        for(var i=0;i<os.length;i++){
+            if(thisOS.match(os[i])){
+                window.location=url;
+            }
+        }
+        var check=navigator.appVersion;
+            if(check.match(/linux/i)){
+                if(check.match(/mobile/i)||check.match(/X11/i)){
+                    window.location=url;
+                }
+            }
+    }
+if(location.search.getParam("from")!="wap"){// 不特定要打开PC端时，默认作判断跳转WAP端
+    mobile_device_detect("/m/index.html");
 }
