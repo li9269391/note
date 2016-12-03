1b5b544f4413069d19ab6095162b5efb6b93dccb
li9269391 <9269391@qq.com>
2016-12-03 10:52:41 +0800

    Create 1.js

---
+++ 片段/mock/1.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+	app     = express(),
+	Mock    = require('mockjs');
+
+/* 测试 */
+app.post('/test/info', function (req, res) {
+	var Random = Mock.Random,
+		data   = Mock.mock({
+			'data': [{
+				'id': '@integer(1, 6)',
+				'name': '@cname'
+			}],
+            'msg':'success|error',
+			'code': '@integer(0, 1)'
+		});
+
+	res.send( JSON.stringify(data, null, 4) );
+});
+
+module.exports = app;
