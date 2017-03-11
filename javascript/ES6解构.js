aef7b4bafa715f3453a846f062f7fa5970365970
chenyongli <9269391@qq.com>
2017-03-11 16:37:04 +0800

    ES6解构

---
+++ javascript/ES6解构.js
@@ -0,0 +1,51 @@
+// 对象默认值解构
+function move1({x=0, y=0} = {}) {
+    trace(x + " " + y + "\n");
+}
+
+function move2({x, y} = { x: 0, y: 0 }) {
+    trace(x + " " + y + "\n");
+}
+
+move1({x: 3, y: 8}); // 3 8
+move1({x: 3}); // 3 0
+move1({}); //0 0
+move1(); //0 0
+move1({z:3})//0 0
+
+trace("\n");
+
+move2({x: 3, y: 8}); //3 8
+move2({x: 3}); //3 undefined
+move2({}); //undefined undefined
+move2(); //0 0
+move2({z:3})//undefined undefined
+
+////////数组解构
+
+function f3([ x, y, ...z], ...w) {
+    trace( x + y + z + w );
+}
+f3( [] ); // undefined undefined [] []
+f3( [1,2,3,4], 5, 6 ); // 1 2 [3,4] [5,6]
+
+function f([ x=2, y=3, z ]) {
+    trace(x + " " +y + " " + z + "\n");
+}
+
+//f();//# Exception: f: cannot coerce undefined to object!
+f([1]);//1 3 undefined
+f([1,2]);//1 2 undefined
+f([1,2,3]);//1 2 3
+f([1,2,3,4]);//1 2 3
+
+// 数组参数解构也可以有默认值，但是如果传入undefined的话，将会报异常，因为undefined不能转换成iterator：
+function f([ x=2, y=3, z ]) {
+    trace(x + " " +y + " " + z + "\n");
+}
+
+//f();//# Exception: f: cannot coerce undefined to object!
+f([1]);//1 3 undefined
+f([1,2]);//1 2 undefined
+f([1,2,3]);//1 2 3
+f([1,2,3,4]);//1 2 3
