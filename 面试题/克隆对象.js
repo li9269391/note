9a8937b5225ac1068fe5b0e1636fa197b5aa95ce
chenyongli <chenyl@mama.cn>
2021-03-15 10:42:45 +0800

    更新注释

--- 面试题/克隆对象.js
+++ 面试题/克隆对象.js
@@ -31,3 +31,17 @@ function clone(Obj) {
         return Obj;
     }
 }
+
+function deepClone(obj) {
+    let result;
+    if (typeof obj === 'object') {
+        result = obj.constructor === Array ? [] : {};
+        // result = obj instaceof Function ? [] : {};
+        for(let i in obj) {
+            result[i] = typeof obj[i] === 'object' ? deepClone(obj[i]) : obj[i];
+        }
+    } else {
+        result = obj;
+    }
+    return result
+}
